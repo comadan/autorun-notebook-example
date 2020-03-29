32f6c1c87c10a32e1b8f2ae2e55c0cf8b6e1599b
Daniel Yoo <comadan@gmail.com>
2020-03-29 11:35:28 -0700

    commit code for automatically running notebooks.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # autorun-notebook-example
 Example Code for Autorunning Jupyter Notebooks
+
+# To Run
+```
+python3 runner.py
+```
---
+++ runner.py
@@ -0,0 +1,21 @@
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+# set filepath to notebook for running 
+notebook_filename = "./notebooks/analysis_notebook.ipynb"
+
+# set filepath to notebook for output
+output_notebook_filename = "./output_notebooks/executed_analysis_notebook.ipynb"
+
+# read in the notebook
+with open(notebook_filename) as f_in:
+    notebook = nbformat.read(f_in, as_version=4)
+
+# create the ExecutePreprocessor class obbject
+notebook_processor = ExecutePreprocessor(timeout=600, kernel_name='python3')
+
+# set the path for the running notebbook.
+notebook_processor.preprocess(nb, {'metadata': {'path': './data/'}})
+
+with open(output_notebook_filename, 'w', encoding='utf-8') as f:
+    nbformat.write(notebook, f)
